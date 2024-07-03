bd53c56fd3ae3823351cff796c6ed96ada34a0eb
Rithik2211 <rithikpranao22@gmail.com>
2024-07-04 00:26:38 +0530

    "Added react-slick and slick-carousel dependencies, updated react and react-dom versions, and modified Testimonial page to use Slider component"

--- next-typescript/app/Practice/Testimonial/page.tsx
+++ next-typescript/app/Practice/Testimonial/page.tsx
@@ -1,11 +1,57 @@
 import Link from 'next/link'
 import React from 'react'
+import Slider from "react-slick";
+import { cardData } from './utils';
+import Image from 'next/image';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Testimonial = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
   return (
-    <div className='text-center py-10 font-Poppins'>
-        <h5 className='text-4xl'>Testimonials</h5>
+    <div className='text-center py-10 font-bold mt-[50px] mb-[50px]'>
+        <h1 className='text-4xl'>Testimonials</h1>
         <Link href={'/'}> Click to Exit</Link>
+        <Slider {...settings}>
+          {cardData?.map((data, index) => (
+            <div key={index}>
+              <Image src={data?.image} alt={data?.title} width={300} height={300} />
+              <h1>{data?.title}</h1>
+            </div>
+          ))}
+        </Slider>
     </div>
   )
 }
---
+++ next-typescript/app/Practice/Testimonial/utils.ts
@@ -0,0 +1,40 @@
+
+interface Card{
+    id : number,
+    title : string,
+    body : string,
+    image : string,
+}
+
+export const cardData : Card[] = [
+    {
+        id : 1,
+        title : 'Sam',
+        body : 'This is the body of card 1',
+        image : '/public/img1.jpg',
+    },
+    {
+        id : 2,
+        title : 'Ram',
+        body : 'This is the body of card 2',
+        image : '/public/img2.jpg',
+    },
+    {
+        id : 3,
+        title : 'Sanjay',
+        body : 'This is the body of card 3',
+        image : '/public/img3.jpg',
+    },
+    {
+        id : 4,
+        title : 'Sahara',
+        body : 'This is the body of card 4',
+        image : '/public/img4.jpg',
+    },
+    {
+        id : 5,
+        title : 'Sushie',
+        body : 'This is the body of card 5',
+        image : '/public/img5.jpg',
+    },
+]
