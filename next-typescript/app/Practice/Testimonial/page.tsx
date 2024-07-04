9463a5767e45a6c77bcaef6ab660d01280b78548
Rithik2211 <rithikpranao22@gmail.com>
2024-07-04 12:54:20 +0530

    "Refactored Testimonial page to remove Slider component and replaced with a grid layout, updated image paths in utils.ts, and modified card/page.tsx to include an Image component from next/image"

--- next-typescript/app/Practice/Testimonial/page.tsx
+++ next-typescript/app/Practice/Testimonial/page.tsx
@@ -1,57 +1,27 @@
 import Link from 'next/link'
 import React from 'react'
-import Slider from "react-slick";
 import { cardData } from './utils';
 import Image from 'next/image';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 const Testimonial = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
   return (
-    <div className='text-center py-10 font-bold mt-[50px] mb-[50px]'>
-        <h1 className='text-4xl'>Testimonials</h1>
-        <Link href={'/'}> Click to Exit</Link>
-        <Slider {...settings}>
-          {cardData?.map((data) => (
-            <div key={data?.id}>
-              <Image src={data?.image} alt={data?.title} width={300} height={300} />
-              <h1>{data?.title}</h1>
-            </div>
-          ))}
-        </Slider>
+    <div className='flex flex-col text-center py-10 font-bold'>
+        <h1 className='text-4xl mb-5'>Testimonials</h1>
+        <Link href={'/'} className='mb-5 text-blue-500'> Click to Exit</Link>
+        <div className='flex justify-center flex-wrap gap-5 p-2 w-full '>
+            {
+              cardData?.map(data => 
+              <div key={data?.id} className='bg-indigo-100 rounded-2xl w-72 h-96'>
+                <Image src={data?.image} alt="data" width={300} height={300} className='rounded-t-2xl object-cover' />
+                <div className='p-4'>
+                  <h1 className='text-xl mt-4'>{data?.title}</h1>
+                  <h2 className='text-lg'>{data?.body}</h2>
+                  <button className='text-blue-500 pt-4 pb-4'>Click for Review</button>
+                </div>
+              </div>
+              )
+            }
+        </div>
     </div>
   )
 }
--- next-typescript/app/Practice/card/page.tsx
+++ next-typescript/app/Practice/card/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -11,7 +12,13 @@ function TailwindCss() {
   return (
     <div className='h-screen w-full flex justify-center items-center p-6'>
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-8 xl:grid-cols-10 grid-rows-8 h-full w-full gap-4'>
-          <div className='col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-4 xl:col-span-6 row-span-2 sm:row-span-2 md:row-span-2 lg:row-span-6 xl:row-span-6 bg-indigo-400 rounded-3xl flex justify-center items-center'>Feature</div>
+          <div className='col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-4 xl:col-span-6 row-span-2 sm:row-span-2 md:row-span-2 lg:row-span-6 xl:row-span-6 bg-indigo-400 rounded-3xl flex justify-center'>
+              <div className='flex flex-col gap-3 text-center'>
+                <Image src={'/img1.jpg'} alt="data" width={404} height={300} className='rounded-t-3xl object-cover'/>
+                <h1>Justin Bento</h1>
+                <h2>I'm very excited to work with you and this project`</h2>
+              </div>
+          </div>
           <div className='col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-3 xl:col-span-3 row-span-1 lg:row-span-2 xl:row-span-2 bg-indigo-400 rounded-3xl flex justify-center items-center'>Artist</div>
           <div className='col-span-1 sm:col-span-2 md:col-span-2 lg:col-span-1 xl:col-span-1 row-span-1 lg:row-span-2 xl:row-span-2 bg-indigo-400 rounded-3xl flex justify-center items-center'>Photos</div>
           <div className='col-span-1 sm:col-span-2 md:col-span-4 lg:col-span-2 xl:col-span-2 row-span-1 md:row-span-2 lg:row-span-4 xl:row-span-4 bg-indigo-400 rounded-3xl flex justify-center items-center'>ArtWork 1</div>
